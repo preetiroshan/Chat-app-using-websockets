7f1460a6dfef156d03acf439f4dc66268f833886
Preeti Roshan <preetiroshan14@gmail.com>
2023-12-03 20:53:49 +0530

    extracted message string from buffer

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import { WebSocketServer } from "ws";
 const server = new WebSocketServer({ port: "3000" });
 server.on("connection", (socket) => {
   socket.on("message", (message) => {
-    console.log(message);
+    const textFromBuffer = Buffer.from(message);
+    console.log(textFromBuffer.toString());
   });
 });
