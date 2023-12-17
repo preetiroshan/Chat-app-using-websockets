e35139e9fccfcd90e704cc758cd66c9c96eb71de
Preeti Roshan <preetiroshan14@gmail.com>
2023-12-17 13:44:13 +0530

    socket io basic setup

--- app/app.js
+++ app/app.js
@@ -1,16 +1,16 @@
-const socket = new WebSocket("ws://localhost:3000");
+const socket = io("ws://localhost:3500");
 function sendMessage(e) {
   console.log("submitting", e);
   e.preventDefault();
   const inputElem = document.querySelector("input");
   let inputVal = inputElem.value;
   if (inputVal) {
-    const list = document.querySelector("ul");
-    const newListItem = document.createElement("li");
-    newListItem.innerText = inputVal;
-    console.log("ws readystate", socket.readyState);
-    socket.send(inputVal);
-    list.appendChild(newListItem);
+    socket.emit("message", inputVal);
+    // const list = document.querySelector("ul");
+    // const newListItem = document.createElement("li");
+    // newListItem.innerText = inputVal;
+    // console.log("ws readystate", socket.readyState);
+    // list.appendChild(newListItem);
     inputElem.value = "";
   }
   inputElem.focus();
@@ -20,7 +20,7 @@ const form = document.querySelector("form");
 form.addEventListener("submit", sendMessage);
 
 // Listen for socket messages
-socket.addEventListener("message", ({ data }) => {
+socket.on("message", (data) => {
   console.log("data in form from socket is", data);
   const li = document.createElement("li");
   li.textContent = data;
